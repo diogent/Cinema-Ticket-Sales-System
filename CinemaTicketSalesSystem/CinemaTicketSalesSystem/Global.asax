d9b5ff3e985269dcb18a4860adbabe71270adf45
Vladislav Malkhasyan <U.Malkhasian@itechart-group.com>
2018-10-31 14:30:26 +0300

    Init Commit

---
+++ CinemaTicketSalesSystem/CinemaTicketSalesSystem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CinemaTicketSalesSystem.MvcApplication" Language="C#" %>
---
+++ CinemaTicketSalesSystem/CinemaTicketSalesSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CinemaTicketSalesSystem.Startup))]
+namespace CinemaTicketSalesSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
