48489ebbeb5cc9a4d639262215a2779ef30bbc85
Vladislav Malkhasyan <U.Malkhasian@itechart-group.com>
2018-11-06 18:06:35 +0300

    sedeed data. Created Home controller

--- CinemaTicketSalesSystem/ApplicationDbMovies/ApplicationDbMovies/Models/MoviesInit.cs
+++ CinemaTicketSalesSystem/ApplicationDbMovies/ApplicationDbMovies/Models/MoviesInit.cs
@@ -18,7 +18,7 @@ namespace ApplicationDbMovies.Models
                 AgeRating = "6+",
                 Description = "A yeti named Migo is convinced that a human known only as Smallfoot is real and has to " +
                 "prove to his tribe that it does exist with the help of Meechee and the S.E.S - Smallfoot Evidentiary Society.",
-                DateOfPremiere = DateTime.Parse("06/11/2018"),
+                DateOfPremiere = DateTime.ParseExact("06.11.2018", "dd.MM.yyyy", null),
                 Actors = new List<Actor>(),
                 Pictures = new List<Picture>(),
                 Producers = new List<Producer>(),
@@ -26,17 +26,17 @@ namespace ApplicationDbMovies.Models
 
             };
 
-            var actor1 = new Actor { FirstName = "Channing", LastName = "Tatum", DateOfBirth = DateTime.Parse("26/04/1980") };
-            var actor2 = new Actor { FirstName = "James", LastName = "Corden", DateOfBirth = DateTime.Parse("22/08/1978") };
-            var actor3 = new Actor { FirstName = "Zendaya", DateOfBirth = DateTime.Parse("01/09/1996") };
-            var actor4 = new Actor { FirstName = "LeBron", LastName = "James", DateOfBirth = DateTime.Parse("30/12/1984") };
+            var actor1 = new Actor { FirstName = "Channing", LastName = "Tatum", DateOfBirth = DateTime.ParseExact("26.04.1980", "dd.MM.yyyy", null) };
+            var actor2 = new Actor { FirstName = "James", LastName = "Corden", DateOfBirth = DateTime.ParseExact("22.08.1978", "dd.MM.yyyy", null) };
+            var actor3 = new Actor { FirstName = "Zendaya", DateOfBirth = DateTime.ParseExact("01.09.1996", "dd.MM.yyyy", null) };
+            var actor4 = new Actor { FirstName = "LeBron", LastName = "James", DateOfBirth = DateTime.ParseExact("30.12.1984", "dd.MM.yyyy", null) };
 
             var genre1 = new Genre { Name = "Animation" };
             var genre2 = new Genre { Name = "Adventure" };
             var genre3 = new Genre { Name = "Comedy" };
 
-            var producer1 = new Producer { FirstName = "Karey", LastName = "Kirkpatrick", DateOfBirth = DateTime.Parse("14/12/1964") };
-            var producer2 = new Producer { FirstName = "Jason", LastName = "Reisig", DateOfBirth = DateTime.Parse("24/08/1970") };
+            var producer1 = new Producer { FirstName = "Karey", LastName = "Kirkpatrick", DateOfBirth = DateTime.ParseExact("14.12.1964", "dd.MM.yyyy", null) };
+            var producer2 = new Producer { FirstName = "Jason", LastName = "Reisig", DateOfBirth = DateTime.ParseExact("24.08.1970", "dd.MM.yyyy", null) };
 
             var pictures1 = new Picture { Url = "/Images/Smallfoot.jpg" };
 
--- CinemaTicketSalesSystem/CinemaTicketSalesSystem/Controllers/HomeController.cs
+++ CinemaTicketSalesSystem/CinemaTicketSalesSystem/Controllers/HomeController.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ApplicationDbMovies.Contexts;
+using ApplicationDbMovies.Models;
 
 namespace CinemaTicketSalesSystem.Controllers
 {
     public class HomeController : Controller
     {
+        ApplicationDbContext db = new ApplicationDbContext();
+
         [HttpGet]
         public ActionResult Index()
         {
+            IEnumerable<Movie> movies = db.Movies;
+            ViewBag.Movies = movies;
             return View();
         }
     }
